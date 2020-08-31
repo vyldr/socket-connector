0bcece82c1ca1934a76fb09c4ef2752cff9004a9
wyld16steven <wyld16steven@gmail.com>
2020-08-30 21:46:59 -0500

    Added Node backend

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const http = require('http');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const httpServer = http.createServer(app);
+
+// Serve static files from a directory
+app.use(express.static('./public'));
+
+// Send anything else to index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+// Start the server
+httpServer.listen(PORT, () => {
+    console.log('Server listening on: ' + PORT);
+});
+
