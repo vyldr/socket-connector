11515ab8b5733e3f3bf260f9bea474e9d80662e8
wyld16steven <wyld16steven@gmail.com>
2020-09-02 15:09:52 -0500

    Added WebSocket

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const WebSocket = require('ws');
 const http = require('http');
 const path = require('path');
 
@@ -20,6 +21,41 @@ app.use(express.static('./public'));
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname + '/public/index.html'));
 });
+
+
+//
+// WebSocket
+//
+
+// Serve WebSocket
+const wss = new WebSocket.Server({
+	server: httpServer,
+	path: '/ws',
+});
+
+// WebSocket connect
+wss.on('connection', (ws) => {
+
+	// Received a message
+	ws.on('message', (message) => {
+		console.log('received:', message);
+
+		// Thank the client for its message
+		var response = 'Thank you for ' + message;
+		ws.send(JSON.stringify(response));
+		console.log('Sent:     ' + response);
+	});
+
+	// Log disconnect
+	ws.on('close', () => {
+		console.log('WebSocket disconnect')
+	});
+
+	// Log connection
+	ws.send(JSON.stringify('Connected'));
+	console.log('WebSocket connect');
+});
+
 
 // Start the server
 httpServer.listen(PORT, () => {
--- frontend/src/components/ControlPage/ControlPage.js
+++ frontend/src/components/ControlPage/ControlPage.js
@@ -3,6 +3,50 @@ import '../../style.css';
 // import './ControlPage.css';
 
 class ControlPage extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			message: '',
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.sendMessage = this.sendMessage.bind(this);
+	}
+
+	componentDidMount() {
+		// Open our WebSocket connection
+		this.ws = new WebSocket('ws://' + document.location.host + '/ws',);
+
+		// The connection is open
+		this.ws.onopen = () => {
+			console.log('connected');
+		};
+
+		// Received a message from the sercer
+		this.ws.onmessage = (event) => {
+			var message = JSON.parse(event.data);
+			console.log('From Server:', message);
+		};
+	}
+
+	// Clean up the WebSocket
+	componentWillUnmount() {
+		console.log('disconnected');
+		this.ws.close();
+	}
+
+	// Send a message to the server
+	sendMessage() {
+		this.ws.send(this.state.message);
+		console.log('To Server:  ', this.state.message);
+		this.setState({ message: '' });
+	}
+
+	// Update the message box text
+	handleChange(event) {
+		this.setState({ message: event.target.value });
+	}
+
 	render() {
 		return (
 			<div className='page'>
@@ -10,8 +54,18 @@ class ControlPage extends React.Component {
 					<div className='formTitle'>
 						<code>Send a message</code>
 					</div>
-					<input placeholder='Message'></input><br />
-					<button className='formButton'>Send</button>
+
+					{/* Message box */}
+					<input placeholder='Message'
+						value={this.state.message}
+						onChange={this.handleChange}>
+					</input><br />
+
+					{/* Send button */}
+					<button className='formButton'
+						onClick={this.sendMessage}>
+						Send
+					</button>
 
 				</div>
 			</div>
